802b288bcaa69da073ee716d33baae92a90d7757
石田岳 <gakuishida@ishidatakenoMacBook-ea.local>
2021-09-11 19:23:31 +0900

    ３回目の修正

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-index.js
+// 少し書いたら試す、を、繰り返す
+'use strict'
+
+{//ここから
+
+// タスクのタイトル
+const showThema = () => {
+    console.log('====================');
+    console.log('現在の持っているタスク一覧');
+    console.log('====================');
+  };
+
+// デフォルトで持っているタスク
+  const tasks = ['掃除', '買い物', '散歩'];
+
+  // タイトルとタスクをまとめて表示
+  const haveTasks = () => {
+    showThema();
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(`${i}：${tasks[i]}`);
+    }
+  };
+
+
+
+
+// タスクを表示させる関数
+const showTasks = () => {
+  // すでにもっているタスクを表示
+  haveTasks();
+
+  //入力画面を表示
+  let inputPrompt = window.prompt('タスクを入力してください');//aは、入力した値
+  window.alert('新しいタスクを追加しました。');
+
+  // 新しいタスクをtasks配列に追加
+  tasks.push(inputPrompt);
+  // 追加したタスクも表示
+  haveTasks();
+
+  // 入力２
+  let optionPrompt = window.prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
+
+}//ここまで、showTasks関数
+
+showTasks();
+
+}//ここから
